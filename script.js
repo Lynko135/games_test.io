fb3d66f3d82cfb697a50b01e97f707ac8b9eea5d
Roman Lynko <lynko.roman090392@gmail.com>
2024-05-06 01:59:44 +0300

    update ref_2

--- script.js
+++ script.js
@@ -3,20 +3,15 @@
 const links = [
    './playables/blocktava/Ref_1_horizontal.html',
    './playables/blocktava/Ref_1_vertical.html',
+   './playables/blocktava/Ref_2_long.html'
 ];
 const root = document.getElementById('root');
 
 const createLinks = (arr, root) => {
     arr.forEach(link => {
         const div = document.createElement('a');
-        // div.classList.add('playable')
         div.textContent = link.slice(2);
-        // div.addEventListener('click', () => {
-        //     window.open(link, '_blank')
-        // })
-
         div.setAttribute('href', link)
-
         root.append(div);
     });
 }
