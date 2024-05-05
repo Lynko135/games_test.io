8d55cb8ed4d42298ecacf0faf52b157596dd74bb
Roman Lynko <lynko.roman090392@gmail.com>
2024-05-06 02:14:03 +0300

    update ref_2

--- script.js
+++ script.js
@@ -3,7 +3,8 @@
 const links = [
    './playables/blocktava/Ref_1_horizontal.html',
    './playables/blocktava/Ref_1_vertical.html',
-   './playables/blocktava/Ref_2_long.html'
+   './playables/blocktava/Ref_B_7clicks.html',
+   './playables/blocktava/Ref_2_30_seconds.html'
 ];
 const root = document.getElementById('root');
 
